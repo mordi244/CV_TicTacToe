dafa9ceea61000717747664a760b5791a956eeb1
mordi244 <mordi244@gmail.com>
2019-07-16 00:06:30 +0300

    change inputs

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 "use strict";
 exports.__esModule = true;
-/* Index File - main file that runs the program */
+/* Index File - runs the program */
 var Player_1 = require("./Classes/Player");
 var Game_1 = require("./Classes/Game");
 var GameStatus_1 = require("./Enums/GameStatus");
-var game = new Game_1.Game(3, 3);
-game.addPlayer(new Player_1.Player('Chandler Bing', 'O'));
-game.addPlayer(new Player_1.Player('Ross Geller', 'X'));
-game.board.print();
+var game = new Game_1.Game(3, 3); //works with any NXN size.
+game.addPlayer(new Player_1.Player('Chandler Bing', 'X'));
+game.addPlayer(new Player_1.Player('Ross Geller', 'O'));
 game.gamePlayers();
 console.log("status : " + GameStatus_1.GameStatus[game.status]);
 console.log(game.nextMove(0, 0));
 console.log(game.nextMove(0, 0));
-console.log(game.nextMove(1, 0));
 console.log(game.nextMove(1, 1));
-console.log(game.nextMove(1, 2));
+console.log(game.nextMove(0, 2));
 console.log(game.nextMove(2, 2));
+console.log(game.nextMove(0, 1));
 console.log(game.nextMove(2, 1));
 console.log("status : " + GameStatus_1.GameStatus[game.status]);
 game.printSummary();
